18736d7e7c27c6970128eb0f414b53472f612004
Alex Hampton <ahampton5158@gmail.com>
2022-02-24 23:05:05 +0900

    set config to heroku deployment

--- config/Database.php
+++ config/Database.php
@@ -1,20 +1,20 @@
 <?php 
     class Database {
-        private $host = '127.0.0.1';
-        private $db_name = 'world';
-        private $username = 'root';
+        // private $host = '127.0.0.1';
+        // private $db_name = 'world';
+        // private $username = 'root';
         //private $password = '';
-        // private $host = 'eanl4i1omny740jw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
-        // private $db_name = 'ie9eexb2umnjakop';
-        // private $username = 'qx9pzx445wuxx8h9';
-        // private $password = getenv('PASSWORD');
+        private $host = 'eanl4i1omny740jw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+        private $db_name = 'ie9eexb2umnjakop';
+        private $username = 'qx9pzx445wuxx8h9';
+        private $password = getenv('PASSWORD');
         private $conn;
 
         public function connect() {
             $this->conn = null;
 
             try {
-                $this->conn = new PDO("mysql:host={$this->host};port=8889;dbname={$this->db_name}", $this->username); 
+                $this->conn = new PDO("mysql:host={$this->host};dbname={$this->db_name}", $this->username, $this->password); 
                 $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             } catch (PDOException $e) {
                 $error_message = 'Database Error: ';
