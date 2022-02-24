8e42e746f15f22ad3000356293e80b79a4d1a294
Alex Hampton <ahampton5158@gmail.com>
2022-02-24 22:53:32 +0900

    update port of database.php

--- config/Database.php
+++ config/Database.php
@@ -14,7 +14,7 @@
             $this->conn = null;
 
             try {
-                $this->conn = new PDO("mysql:host={$this->host};dbname={$this->db_name}", $this->username); 
+                $this->conn = new PDO("mysql:host={$this->host};port=8889;dbname={$this->db_name}", $this->username); 
                 $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             } catch (PDOException $e) {
                 $error_message = 'Database Error: ';
