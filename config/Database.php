316c621a95a3281b21901108b559c0f108fe0e44
Alex Hampton <ahampton5158@gmail.com>
2022-02-24 22:33:27 +0900

    Update Database.php

--- config/Database.php
+++ config/Database.php
@@ -1,16 +1,16 @@
 <?php 
     class Database {
-        private $host = 'localhost';
-        private $db_name = 'world';
-        private $username = 'root';
-        //private $password = 'pa55word';
+        private $host = 'eanl4i1omny740jw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+        private $db_name = 'ie9eexb2umnjakop';
+        private $username = 'qx9pzx445wuxx8h9';
+        private $password = getenv('PASSWORD');
         private $conn;
 
         public function connect() {
             $this->conn = null;
 
             try {
-                $this->conn = new PDO("mysql:host={$this->host};dbname={$this->db_name}", $this->username); 
+                $this->conn = new PDO("mysql:host={$this->host};dbname={$this->db_name}", $this->username, $this->password); 
                 $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             } catch (PDOException $e) {
                 $error_message = 'Database Error: ';
